6fc89ebe29f0e7a3ca087d66675edb4485009cd2
Nicolai Maisch <nicolai.maisch@isw.uni-stuttgart.de>
2024-12-20 11:51:29 +0100

    added Readme and Script

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # ITArch-Uebung5-A2
+
+- Laden Sie die Übung herunter und öffnen Sie das Python Programm. 
+
+- Tragen Sie die IP-Adresse des ISW-Brokers ein. 
+
+- Geben Sie das Topic an, unter dem Nachrichten übermittelt werden sollen. 
+
+- Geben Sie die Nachricht selbst an. 
+
+- Veröffentlichen Sie verschiedene Nachrichten über den öffentlichen Broker und den ISW-Broker durch Ausführen des Programms. 
---
+++ publisher.py
@@ -0,0 +1,37 @@
+import time
+
+import paho.mqtt.client as mqtt
+
+############################################################
+# Relevanter Teil fuer die Uebung
+# Hier die MQTT settings eintragen
+ONLINE_BROKER = "test.mosquitto.org"
+ISW_BROKER = ""
+PORT = 1883
+TOPIC = "isw/ITArch/Team-TEAMNAME"
+MESSAGE = "Hello from Team TEAMNAME"
+
+#Ab hier muss nichts mehr angepassst werden
+############################################################
+
+
+# Create an MQTT client instance
+client = mqtt.Client()
+
+# Connect to the broker
+client.connect(BROKER, PORT, 60)
+
+# Publish the message
+client.publish(TOPIC, MESSAGE)
+print(f"Published message '{MESSAGE}' to topic '{TOPIC}'.")
+
+# In case of continous publishing 
+#while True:
+#    # Publish the message
+#    client.publish(TOPIC, MESSAGE)
+#    print(f"Published message '{MESSAGE}' to topic '{TOPIC}'.")
+#    time.sleep(5)
+
+# Disconnect from the broker
+client.disconnect()
+
